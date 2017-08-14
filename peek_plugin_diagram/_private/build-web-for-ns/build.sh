bcf3bbebc19d70e6bb1b5ddc1c32d7d83943c61a
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-08-14 11:38:18 +1000

    Updated to vortexjs@0.8.10

--- peek_plugin_diagram/_private/build-web-for-ns/build.sh
+++ peek_plugin_diagram/_private/build-web-for-ns/build.sh
@@ -7,5 +7,9 @@ ng build -prod --no-aot
 
 [ -d dist ]
 
-rm ../web-dist/*
-mv dist/* ../web-dist/
+rm ../dist-web/*
+mv dist/* ../dist-web/
+
+sed -i 's,src=",src="/peek_plugin_diagram/web_dist/,g' ../dist-web/index.html
+sed -i 's,href=",href="/peek_plugin_diagram/web_dist/,g' ../dist-web/index.html
+
