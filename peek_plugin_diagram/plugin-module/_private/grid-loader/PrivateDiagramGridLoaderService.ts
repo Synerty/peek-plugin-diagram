99a1c61b753ae14e779b96a09463b71b4a46c6b0
Jarrod Chesney <jarrod.chesney@synerty.com>
2023-02-26 21:00:34 +1000

    PEEK-1904 Removed vortexOnline -> askServerForUpdates subscription

--- peek_plugin_diagram/plugin-module/_private/grid-loader/PrivateDiagramGridLoaderService.ts
+++ peek_plugin_diagram/plugin-module/_private/grid-loader/PrivateDiagramGridLoaderService.ts
@@ -219,15 +219,6 @@ export class PrivateDiagramGridLoaderService extends PrivateDiagramGridLoaderSer
             .subscribe((payloadEnvelope: PayloadEnvelope) =>
                 this.processChunksFromServer(payloadEnvelope)
             );
-
-        // If the vortex service comes back online, update the watch grids.
-        this.vortexStatusService.isOnline
-            .pipe(filter((isOnline) => isOnline == true))
-            .pipe(takeUntil(this.onDestroyEvent))
-            .subscribe(() => {
-                this.loadGrids({}, this.lastWatchedGridKeys);
-                this.askServerForUpdates();
-            });
     }
 
     private areWeTalkingToTheServer(): boolean {
